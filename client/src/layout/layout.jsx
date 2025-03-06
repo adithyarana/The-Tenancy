087a5544022031d4c6ca1823c1b98a46bec8b2d6
adithya <adithyarana447@gmail.com>
2025-03-06 21:46:43 +0530

    home page change

--- client/src/layout/layout.jsx
+++ client/src/layout/layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Homepage/navbar.jsx';
 
 function Applayout() {
   return (
     <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
       <Navbar />
-      <main className="flex-grow  w-full">  {/* Removed px-4 to avoid side padding */}
+      <main className="flex-grow  w-full">  
         <Outlet />
       </main>
      
